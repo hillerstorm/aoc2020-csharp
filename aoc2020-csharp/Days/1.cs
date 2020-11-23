235e7f67ea90d28ec235fa5dab59be1c10d1d9f1
Johan Hillerström <progr@mmer.nu>
2020-11-23 13:02:05 +0100

    Add base project files in preparation of 2020

---
+++ aoc2020-csharp/Days/1.cs
@@ -0,0 +1,9 @@
+﻿namespace aoc2020.Days
+{
+    public static class Day01
+    {
+        public static void Run(string input)
+        {
+        }
+    }
+}
---
+++ aoc2020-csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using aoc2020;
+using aoc2020.Days;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("No day given");
+    return;
+}
+
+var days = new Dictionary<int, Action<string>>
+{
+    {1, Day01.Run}
+};
+
+var day = int.Parse(args[0]);
+var (input, error) = await InputFetcher.GetInput(day);
+if (string.IsNullOrWhiteSpace(error))
+    days[day](input);
+else
+    Console.WriteLine(error);
