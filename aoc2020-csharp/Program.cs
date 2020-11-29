af32a0649c8b8152c9eeae7fd3774d15f6c2f7eb
Johan Hillerström <progr@mmer.nu>
2020-11-29 21:30:20 +0100

    Add checks to not download input prematurely

--- aoc2020-csharp/Program.cs
+++ aoc2020-csharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using aoc2020;
 using aoc2020.Days;
 
@@ -19,6 +20,20 @@ if (!int.TryParse(args[0], out var day) || day <= 0 || day > days.Length)
     Console.WriteLine($"Invalid input, must be a day between 1-{days.Length}");
     return;
 }
+
+var info = TimeZoneInfo.FindSystemTimeZoneById(
+    RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? "Eastern Standard Time"
+        : "America/New_York"
+);
+var now = new DateTimeOffset(TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, info), info.BaseUtcOffset);
+var then = new DateTimeOffset(2020, 12, day, 0, 0, 0, info.BaseUtcOffset);
+if (now < then)
+{
+    Console.WriteLine($"Day {day} can't be started yet, {then - now:d\\:hh\\:mm\\:ss} left");
+    return;
+}
+
 
 var (input, error) = await day.GetInput();
 if (!string.IsNullOrWhiteSpace(error))
