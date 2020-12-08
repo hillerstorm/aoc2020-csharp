877c9d691c434934466d5e0a2258ff10b05160fa
Johan Hillerström <progr@mmer.nu>
2020-12-08 12:07:22 +0100

    Add day 8

---
+++ aoc2020-csharp/Days/Day08.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedParameter.Global
+// ReSharper disable StringIndexOfIsCultureSpecific.1
+
+namespace aoc2020.Days
+{
+    public class Day08 : IDay
+    {
+        public (Func<string> Part1, Func<string> Part2) Parts(string input)
+        {
+            var lines = input.SplitLines();
+            return (
+                () => Part1(lines).ToString(),
+                () => Part2(lines).ToString()
+            );
+        }
+
+        public static int Part1(string[] input) =>
+            Run(input)!.Value;
+
+        public static int Part2(string[] input)
+        {
+            var changed = new List<int>();
+            while (true)
+            {
+                var res = Run(input, changed);
+                if (res.HasValue)
+                    return res.Value;
+            }
+        }
+
+        private static int? Run(IReadOnlyList<string> input, ICollection<int>? changed = null)
+        {
+            var acc = 0;
+            int? changedIndex = null;
+            var seen = new List<int>();
+            for (var i = 0; i < input.Count;)
+            {
+                if (seen.Contains(i))
+                    return changed != null ? null : acc;
+
+                seen.Add(i);
+                var (op, value, _) = input[i % input.Count].Split(" ");
+                switch (op)
+                {
+                    case "acc":
+                        acc += int.Parse(value);
+                        break;
+                    case "jmp" or "nop" when changed != null && !changedIndex.HasValue && !changed.Contains(i):
+                        changed.Add(i);
+                        changedIndex = i;
+                        if (op == "nop")
+                        {
+                            i += int.Parse(value);
+                            continue;
+                        }
+
+                        break;
+                    case "jmp":
+                        i += int.Parse(value);
+                        continue;
+                }
+
+                i++;
+            }
+
+            return acc;
+        }
+    }
+}
---
+++ aoc2020.Tests/Day08Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using aoc2020.Days;
+using Xunit;
+
+namespace aoc2020.Tests
+{
+    public class Day08Tests
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var assertions = new[]
+            {
+                ("Inputs/08.txt", 5),
+            };
+            assertions.ForEach(((string Input, int Expected) x) =>
+                Assert.Equal(x.Expected, Day08.Part1(File.ReadAllText(x.Input).SplitLines())));
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var assertions = new[]
+            {
+                ("Inputs/08.txt", 8),
+            };
+            assertions.ForEach(((string Input, int Expected) x) =>
+                Assert.Equal(x.Expected, Day08.Part2(File.ReadAllText(x.Input).SplitLines())));
+        }
+    }
+}
