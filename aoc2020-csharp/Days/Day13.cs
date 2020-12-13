ff96beced30c48a9b021be97079cf7bb491d01be
Johan Hillerström <progr@mmer.nu>
2020-12-13 21:02:41 +0100

    Add day 13

---
+++ aoc2020-csharp/Days/Day13.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedParameter.Global
+
+namespace aoc2020.Days
+{
+    public class Day13 : IDay
+    {
+        public (Func<string> Part1, Func<string> Part2) Parts(string input)
+        {
+            var lines = input.SplitLines();
+            return (
+                () => Part1(lines).ToString(),
+                () => Part2(lines[1]).ToString()
+            );
+        }
+
+        public static int Part1(string[] input)
+        {
+            var schedule = ParseSchedule(input[1]);
+            var earliest = int.Parse(input[0]);
+            for (var i = earliest;; i++)
+                foreach (var (id, _) in schedule)
+                    if (i % id == 0)
+                        return id * (i - earliest);
+        }
+
+        public static long Part2(string input)
+        {
+            var schedule = ParseSchedule(input);
+            long t = 0;
+            long product = 1;
+            foreach (var (id, index) in schedule)
+            {
+                // Loop through the bus schedule and add the current running product
+                // until the the first valid timestamp is found for the given bus.
+                // Once the first timestamp is found, that condition will always be
+                // true, since we keep adding that number every iteration when looking
+                // for the first valid timestamp for the next bus.
+                while ((t + index) % id != 0)
+                    t += product;
+
+                product *= id;
+            }
+
+            return t;
+        }
+
+        private static (int Id, int Index)[] ParseSchedule(string schedule) =>
+            schedule
+                .Split(",")
+                .Select((x, i) => (Known: int.TryParse(x, out var y), Id: y, Index: i))
+                .Where(x => x.Known)
+                .Select(x => (x.Id, x.Index))
+                .ToArray();
+    }
+}
---
+++ aoc2020.Tests/Day13Tests.cs
@@ -0,0 +1,35 @@
+﻿using aoc2020.Days;
+using Xunit;
+
+namespace aoc2020.Tests
+{
+    public class Day13Tests
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var assertions = new[]
+            {
+                ("939 7,13,x,x,59,x,31,19", 295),
+            };
+            assertions.ForEach(((string Input, int Expected) x) =>
+                Assert.Equal(x.Expected, Day13.Part1(x.Input.Split(" "))));
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var assertions = new[]
+            {
+                ("7,13,x,x,59,x,31,19", 1068781L),
+                ("17,x,13,19", 3417L),
+                ("67,7,59,61", 754018L),
+                ("67,x,7,59,61", 779210L),
+                ("67,7,x,59,61", 1261476L),
+                ("1789,37,47,1889", 1202161486L),
+            };
+            assertions.ForEach(((string Input, long Expected) x) =>
+                Assert.Equal(x.Expected, Day13.Part2(x.Input)));
+        }
+    }
+}
