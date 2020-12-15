3e109c1f551f1aa9715b0473e0e9a5761479f56a
Johan Hillerström <progr@mmer.nu>
2020-12-15 16:57:49 +0100

    Add day 15

---
+++ aoc2020-csharp/Days/Day15.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedParameter.Global
+
+namespace aoc2020.Days
+{
+    public class Day15 : IDay
+    {
+        public (Func<string> Part1, Func<string> Part2) Parts(string input)
+        {
+            var parsed = input.SplitAsInt(",");
+            return (
+                () => Part1(parsed.AsSpan()).ToString(),
+                () => Part2(parsed.AsSpan()).ToString()
+            );
+        }
+
+        public static int Part1(Span<int> input) =>
+            Solve(input, 2020);
+
+        public static int Part2(Span<int> input) =>
+            Solve(input, 30000000);
+
+        private static int Solve(Span<int> input, int max)
+        {
+            var seen = new Dictionary<int, int>();
+            var numbers = new Span<int>(new int[max]);
+            var len = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (i != input.Length - 1)
+                    seen[input[i]] = i;
+                numbers[i] = input[i];
+                len++;
+            }
+
+            while (len < max)
+            {
+                var lastNum = numbers[len - 1];
+                var lastIndex = seen.TryGetValue(lastNum, out var idx) ? idx : -1;
+                seen[lastNum] = len - 1;
+
+                if (lastIndex == -1)
+                    lastNum = 0;
+                else
+                    lastNum = len - 1 - lastIndex;
+                numbers[len] = lastNum;
+                len++;
+            }
+
+            return numbers[^1];
+        }
+    }
+}
---
+++ aoc2020.Tests/Day15Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using aoc2020.Days;
+using Xunit;
+
+namespace aoc2020.Tests
+{
+    public class Day15Tests
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var assertions = new[]
+            {
+                ("0,3,6", 436),
+                ("1,3,2", 1),
+                ("2,1,3", 10),
+                ("1,2,3", 27),
+                ("2,3,1", 78),
+                ("3,2,1", 438),
+                ("3,1,2", 1836),
+            };
+            assertions.ForEach(((string Input, int Expected) x) =>
+                Assert.Equal(x.Expected, Day15.Part1(x.Input.SplitAsInt(",").AsSpan())));
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var assertions = new[]
+            {
+                ("0,3,6", 175594),
+                ("1,3,2", 2578),
+                ("2,1,3", 3544142),
+                ("1,2,3", 261214),
+                ("2,3,1", 6895259),
+                ("3,2,1", 18),
+                ("3,1,2", 362),
+            };
+            assertions.ForEach(((string Input, int Expected) x) =>
+                Assert.Equal(x.Expected, Day15.Part2(x.Input.SplitAsInt(",").AsSpan())));
+        }
+    }
+}
