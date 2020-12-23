517cbba5595710e96bd1878e55eb058753496b9b
Johan Hillerström <progr@mmer.nu>
2020-12-23 12:58:55 +0100

    Add day 21

---
+++ aoc2020-csharp/Days/Day21.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedParameter.Global
+
+namespace aoc2020.Days
+{
+    public class Day21 : IDay
+    {
+        public (Func<string> Part1, Func<string> Part2) Parts(string input)
+        {
+            var lines = input.SplitLines();
+            return (
+                () => Part1(lines).ToString(),
+                () => Part2(lines)
+            );
+        }
+
+        public static int Part1(IEnumerable<string> input)
+        {
+            var (lines, possibleAllergens) = ParseValues(input);
+
+            var awd = possibleAllergens
+                .Where(x => x.Value.Count == 0)
+                .Select(x => lines.Count(y => y.Ingredients.Contains(x.Key)))
+                .Sum();
+
+            return awd;
+        }
+
+        private static ((string[] Ingredients, string[] Allergens)[] Lines, Dictionary<string, List<string>> PossibleAllergens)
+            ParseValues(IEnumerable<string> input)
+        {
+            var lines = input.Select(x =>
+            {
+                var line = x.TrimEnd(')');
+                var (ingredients, allergens, _) = line.Split(" (contains ", StringSplitOptions.RemoveEmptyEntries);
+                var allIngredients = ingredients.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var allAllergens = allergens.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                return (Ingredients: allIngredients, Allergens: allAllergens);
+            }).ToArray();
+
+            var uniqueIngredients = lines.SelectMany(x => x.Ingredients).Distinct().ToArray();
+            var uniqueAllergens = lines.SelectMany(x => x.Allergens).Distinct().ToArray();
+            var possibleAllergens = uniqueIngredients
+                .ToDictionary(
+                    x => x,
+                    x => uniqueAllergens
+                        .Where(y => lines
+                            .Any(p => p.Allergens.Contains(y) && p.Ingredients.Contains(x))
+                        )
+                        .ToList()
+                );
+
+            foreach (var ingredient in uniqueIngredients)
+            {
+                var allergens = possibleAllergens[ingredient].ToArray();
+                foreach (var allergen in allergens)
+                {
+                    var notMatching = lines
+                        .Where(x => x.Allergens.Contains(allergen) && !x.Ingredients.Contains(ingredient))
+                        .ToArray();
+
+                    if (notMatching.Length == 0)
+                        continue;
+
+                    possibleAllergens[ingredient].Remove(allergen);
+                }
+            }
+
+            return (lines, possibleAllergens);
+        }
+
+        public static string Part2(IEnumerable<string> input)
+        {
+            var (_, possibleAllergens) = ParseValues(input);
+
+            var withIngredients = possibleAllergens
+                .Where(x => x.Value.Count > 0)
+                .ToArray();
+
+            while (withIngredients.Any(x => x.Value.Count > 1))
+            {
+                var singles = withIngredients.Where(x => x.Value.Count == 1).ToArray();
+                var multis = withIngredients.Where(x => x.Value.Count > 1).ToArray();
+                foreach (var (_, ingredients) in singles)
+                    foreach (var (_, values) in multis)
+                        values.Remove(ingredients[0]);
+            }
+
+            return string.Join(
+                ",",
+                withIngredients
+                    .OrderBy(x => x.Value[0])
+                    .Select(x => x.Key)
+            );
+        }
+    }
+}
---
+++ aoc2020.Tests/Day21Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using aoc2020.Days;
+using Xunit;
+
+namespace aoc2020.Tests
+{
+    public class Day21Tests
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var assertions = new[]
+            {
+                ("Inputs/21.txt", 5),
+            };
+            assertions.ForEach(((string Input, int Expected) x) =>
+                Assert.Equal(x.Expected, Day21.Part1(File.ReadAllText(x.Input).SplitLines())));
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var assertions = new[]
+            {
+                ("Inputs/21.txt", "mxmxvkd,sqjhc,fvjkl"),
+            };
+            assertions.ForEach(((string Input, string Expected) x) =>
+                Assert.Equal(x.Expected, Day21.Part2(File.ReadAllText(x.Input).SplitLines())));
+        }
+    }
+}
