6f385e40f4d8f3d035f557f31fa6bc0e7943affa
Johan Hillerström <progr@mmer.nu>
2020-12-27 23:37:13 +0100

    Add day 25

---
+++ aoc2020-csharp/Days/Day25.cs
@@ -0,0 +1,56 @@
+﻿using System;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedParameter.Global
+
+namespace aoc2020.Days
+{
+    public class Day25 : IDay
+    {
+        public (Func<string> Part1, Func<string> Part2) Parts(string input)
+        {
+            var lines = input.SplitAsInt();
+            return (
+                () => Part1(lines).ToString(),
+                () => string.Empty
+            );
+        }
+
+        public static long Part1(int[] input)
+        {
+            var (cardPkey, doorPkey, _) = input;
+            var doorLoopSize = GetLoopSize(doorPkey);
+            return Transform(doorLoopSize, cardPkey);
+        }
+
+        private static int GetLoopSize(in long cardPkey)
+        {
+            var loopSize = 1;
+            const int subjectNumber = 7;
+            var previousValue = 1L;
+            while (true)
+            {
+                var val = previousValue * subjectNumber;
+                val %= 20201227;
+
+                if (val == cardPkey)
+                    return loopSize;
+
+                previousValue = val;
+                loopSize++;
+            }
+        }
+
+        private static long Transform(int loopSize, int subjectNumber)
+        {
+            var val = 1L;
+
+            for (var i = 0; i < loopSize; i++)
+            {
+                val *= subjectNumber;
+                val %= 20201227;
+            }
+
+            return val;
+        }
+    }
+}
---
+++ aoc2020.Tests/Day25Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using aoc2020.Days;
+using Xunit;
+
+namespace aoc2020.Tests
+{
+    public class Day25Tests
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var assertions = new[]
+            {
+                ("Inputs/25.txt", 14897079L),
+            };
+            assertions.ForEach(((string Input, long Expected) x) =>
+                Assert.Equal(x.Expected, Day25.Part1(File.ReadAllText(x.Input).SplitAsInt())));
+        }
+    }
+}
